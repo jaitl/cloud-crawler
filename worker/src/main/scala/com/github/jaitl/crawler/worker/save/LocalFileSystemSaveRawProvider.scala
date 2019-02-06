ebe5442e303d62344a8f6dd739447df376600963
Anton V Chunikhin <kukowa@gmail.com>
2019-02-06 16:12:16 +0300

    Merge branch 'add_scalatest' of https://github.com/Jaitl/cloud-crawler into add_scalatest
    # Conflicts:
    #	build.gradle

--- worker/src/main/scala/com/github/jaitl/crawler/worker/save/LocalFileSystemSaveRawProvider.scala
+++ worker/src/main/scala/com/github/jaitl/crawler/worker/save/LocalFileSystemSaveRawProvider.scala
@@ -1,10 +1,24 @@
 package com.github.jaitl.crawler.worker.save
 
+import java.io.File
+import java.io.BufferedWriter
+import java.io.FileWriter
+
 import com.github.jaitl.crawler.models.task.Task
 import com.github.jaitl.crawler.worker.crawler.CrawlResult
 
 import scala.concurrent.Future
 
 class LocalFileSystemSaveRawProvider(val path: String) extends SaveRawProvider {
-  override def save(raw: Seq[(Task, CrawlResult)]): Future[Unit] = ???
+  override def save(raw: Seq[(Task, CrawlResult)]): Future[Unit] = {
+    raw.foreach(r => {
+      val fileName = path.concat(r._1.id).concat(r._1.taskType).concat(r._1.taskData)
+      val file = new File(fileName)
+      val bw = new BufferedWriter(new FileWriter(file))
+      bw.write(r._2.data)
+      bw.close()
+      logger.debug(s"Saving crawler result to: $fileName")
+    })
+    Future.successful(())
+  }
 }
--- worker/src/main/scala/com/github/jaitl/crawler/worker/save/SaveRawProvider.scala
+++ worker/src/main/scala/com/github/jaitl/crawler/worker/save/SaveRawProvider.scala
@@ -2,9 +2,10 @@ package com.github.jaitl.crawler.worker.save
 
 import com.github.jaitl.crawler.models.task.Task
 import com.github.jaitl.crawler.worker.crawler.CrawlResult
+import com.typesafe.scalalogging.StrictLogging
 
 import scala.concurrent.Future
 
-trait SaveRawProvider {
+trait SaveRawProvider extends StrictLogging {
   def save(raw: Seq[(Task, CrawlResult)]): Future[Unit]
 }
