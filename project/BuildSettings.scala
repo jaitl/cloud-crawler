54510feae56644f272d36d2004387fab2029bac5
Jaitl <jaitl@outlook.com>
2019-06-19 19:43:59 +0300

    migrate to sbt

---
+++ project/BuildSettings.scala
@@ -0,0 +1,32 @@
+import sbt.Keys._
+import sbt._
+import scoverage.ScoverageSbtPlugin.autoImport._
+
+object BuildSettings {
+  lazy val commonSettings: Seq[Setting[_]] = Seq(
+    scalaVersion := "2.12.8",
+    organization := "com.github.jaitl",
+    organizationName := "JaitlApp",
+    resolvers += "jitpack" at "https://jitpack.io",
+    scalacOptions := Seq(
+      "-encoding",
+      "UTF-8", // source files are in UTF-8
+      "-deprecation", // warn about use of deprecated APIs
+      "-unchecked", // warn about unchecked type parameters
+      "-feature", // warn about misused language features
+      "-language:higherKinds", // allow higher kinded types without `import scala.language.higherKinds`
+      "-Xlint", // enable handy linter warnings
+      // "-Xfatal-warnings", // turn compiler warnings into errors
+      "-Ypartial-unification" // allow the compiler to unify type constructors of different arities
+    ),
+    coverageExcludedPackages := Seq(
+      "<empty>",
+      ".*WorkerApp.*",
+      ".*MasterApp.*",
+      ".*MongoQueueTaskProvider.*",
+      ".*QueueTaskProviderFactory.*",
+      ".*\\.simple\\.worker\\..*",
+      ".*\\.crawler\\.models\\..*"
+    ).mkString(";")
+  )
+}
