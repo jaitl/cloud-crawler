b0052ab02bebb64280558c870b4f832905a8b02d
Anton V Chunikhin <yajcms@gmail.com>
2020-09-25 20:20:16 +0200

    [no-issue] SQL

--- project/ProtobufSettings.scala
+++ project/ProtobufSettings.scala
@@ -2,12 +2,10 @@ import sbt.Keys._
 import sbt._
 import sbtprotoc.ProtocPlugin.autoImport._
 
-import scala.sys.process.Process
 object ProtobufSettings {
   lazy val protoSettings = Seq(
     PB.targets in Compile := Seq(
       scalapb.gen() -> (sourceManaged in Compile).value
-    ),
-    PB.runProtoc in Compile := (args => Process("/usr/local/bin/protoc", args)!)
+    )
   )
 }
